6251da72c7c3bd49365f110d112c6ab59e982ae5
Marius <m.latinis@gmail.com>
2021-05-22 23:35:25 +0300

    Minor readability improvement

--- fast_evaluator.py
+++ fast_evaluator.py
@@ -6,13 +6,12 @@ def ReadEvaluationTable():
     table_file = open("evaluation_table.txt")
     table = {}
     for line in table_file:
-        rank, hand = line.split()
-        table[hand] = int(rank)
+        score, ranks = line.split()
+        table[ranks] = int(score)
     table_file.close()
     return table
 
 def EvaluateWithTable(cards, table):
-    ranks = SortedRanks(cards)
-    string = "".join([RANKS[r] for r in ranks])
-    score = table[string]
+    ranks = "".join([RANKS[r] for r in SortedRanks(cards)])
+    score = table[ranks]
     return score + FLUSH_SCORE if Flush(cards) else score
