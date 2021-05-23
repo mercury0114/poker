c168482ea074c4e4a273dc4024f9f9caeea6a8e7
Marius <m.latinis@gmail.com>
2021-05-23 23:39:20 +0300

    Shortening read of table file

--- fast_evaluator.py
+++ fast_evaluator.py
@@ -3,13 +3,9 @@ from evaluator import RANKS, SUITS, SortedRanks, Flush
 FLUSH_SCORE = 5863
 
 def ReadEvaluationTable():
-    table = {}
-    for line in open("evaluation_table.txt"):
-        score, ranks = line.split()
-        table[ranks] = int(score)
-    return table
+    return {row.split()[1] : int(row.split()[0]) for row in open("evaluation_table.txt")}
 
 def EvaluateWithTable(cards, table):
-    ranks = "".join([RANKS[r] for r in SortedRanks(cards)])
+    ranks = "".join([RANKS[rank] for rank in SortedRanks(cards)])
     score = table[ranks]
     return score + FLUSH_SCORE if Flush(cards) else score
