253b4295a595a9a943bdc53b245ad55354300a62
Marius Latinis <m.latinis@gmail.com>
2022-07-16 22:28:55 +0200

    moving determine_winners method to another file

--- simulator.py
+++ simulator.py
@@ -1,10 +1,8 @@
-from itertools import combinations
 from random import shuffle
 
 from cards_reader import check_cards_are_valid
-from evaluator import evaluate_with_table
+from utils import determine_winners
 from utils import get_free_cards
-from utils import get_used_cards
 
 SIMULATION_COUNT = 10000
 NEVER_LOOSE = "never lose"
@@ -22,17 +20,6 @@ def simulate_game(board_cards, players, free_cards):
     chosen_players = {p: choose_unknown_cards(players[p], free_cards)
                       for p in players}
     return chosen_board, chosen_players
-
-
-def determine_winners(board_cards, players, evaluation_table):
-    best_hands = {}
-    for player in players:
-        seven_cards = get_used_cards(board_cards, {player: players[player]})
-        evaluations = [evaluate_with_table(hand, evaluation_table)
-                       for hand in combinations(seven_cards, 5)]
-        best_hands[player] = max(evaluations)
-    winning_hand = max(best_hands.values())
-    return [player for player in players if best_hands[player] == winning_hand]
 
 
 def perform_simulations(board_cards, players, evaluation_table):
--- utils.py
+++ utils.py
@@ -1,7 +1,20 @@
+from itertools import combinations
 from itertools import product
 
 from evaluator import RANKS
 from evaluator import SUITS
+from evaluator import evaluate_with_table
+
+
+def determine_winners(board_cards, players, evaluation_table):
+    best_hands = {}
+    for player in players:
+        seven_cards = get_used_cards(board_cards, {player: players[player]})
+        evaluations = [evaluate_with_table(hand, evaluation_table)
+                       for hand in combinations(seven_cards, 5)]
+        best_hands[player] = max(evaluations)
+    winning_hand = max(best_hands.values())
+    return [player for player in players if best_hands[player] == winning_hand]
 
 
 def get_free_cards(board_cards, players):
