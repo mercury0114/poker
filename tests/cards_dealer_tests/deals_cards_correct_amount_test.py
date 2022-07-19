e7db7bf235134687b6cddd1b1288fa0de70a30f4
Marius Latinis <m.latinis@gmail.com>
2022-07-19 23:36:11 +0200

    refactoring deals_cards_correct_ammount_test

--- tests/cards_dealer_tests/deals_cards_correct_amount_test.py
+++ tests/cards_dealer_tests/deals_cards_correct_amount_test.py
@@ -7,6 +7,6 @@ from cards_reader import check_cards_are_valid
 for number in range(2, 8):
     board_cards, players = deal_cards(number)
     check_cards_are_valid(board_cards, players)
-    assert len(players) == number
+    assert len(players) == number, f"{len(players)} != {number}"
 
 print("Passed")
