81aa25afbe6dc2eb4ba23edd158870b09995626d
Marius Latinis <m.latinis@gmail.com>
2022-08-07 00:07:17 +0200

    hands summary as a separate function

--- casino/table.py
+++ casino/table.py
@@ -1,10 +1,29 @@
 from cards.evaluator import read_evaluation_table
 from casino.hand import play_hand_return_wins
 
+SMALL_BLIND_POSITION = 0
+
 
 def play_next_hand():
     print("Type 'y' to play next hand")
     return input() == "y"
+
+
+def folded_hand(position, win):
+    if position == SMALL_BLIND_POSITION:
+        return win == -1
+    # TODO(mercury0114): handle big blind position correctly
+    return win == 0
+
+
+def print_statistics(hands_count, hands_folded, balance):
+    if not hands_count:
+        return
+    print(f"{hands_count} hands")
+    print(f"hands folded: {hands_folded}")
+    print(f"total balance:   {balance}")
+    average = {name: balance[name] // hands_count for name in balance}
+    print(f"average balance: {average}")
 
 
 class Table:
@@ -14,18 +33,15 @@ class Table:
         self.table = read_evaluation_table()
 
     def play(self):
-        hands_played = 0
+        hands_count = 0
         balance = {player.name: 0 for player in self.players}
+        hands_folded = {player.name: 0 for player in self.players}
         while play_next_hand():
             wins = play_hand_return_wins(self.players, self.table)
             for i, win in enumerate(wins):
                 balance[self.players[i].name] += win
-            hands_played += 1
+                hands_folded[self.players[i].name] += folded_hand(i, win)
+            hands_count += 1
             if self.rotate_positions:
                 self.players = self.players[1:] + self.players[:1]
-        if not hands_played:
-            return
-        print(f"played {hands_played} hands")
-        print(f"total balance:   {balance}")
-        average = {name: balance[name] // hands_played for name in balance}
-        print(f"average balance: {average}")
+        print_statistics(hands_count, hands_folded, balance)
